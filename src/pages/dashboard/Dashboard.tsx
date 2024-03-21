71c80264a8979ed86558c8154d7cc9eac548ce27
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-21 12:58:03 +0100

    storybook added

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,26 @@
+import { Box, Grid, Paper } from "@mui/material";
 import React from "react";
+import classes from "@/styles/Dashboard.module.scss";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Box>
+      <Grid container gap={2} className={classes.topCardsContainer}>
+        <Grid>
+          <Paper className={classes.dataCard}>xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={classes.dataCard}>xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={classes.dataCard}>xs=4</Paper>
+        </Grid>
+      </Grid>
+      <Grid xs={12} marginY={2}>
+        <Paper className={classes.dataCard}>xs=8</Paper>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Dashboard;
