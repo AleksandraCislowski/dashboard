c09502541381e3c64fa6fc4737769bfd05c3329f
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-23 20:37:09 +0100

    transactions per day

---
+++ src/components/Dashboard/TransactionsPerDay/TransactionsPerDay.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import classes from "@/styles/TransactionsPerDay.module.scss";
+import { Card, Grid, Paper } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/system";
+import DataChart from "@/components/DataChart/DataChart";
+import { lineChartData } from "@/components/mockData";
+
+export type TransactionCardType = {
+  title: string;
+  value: string;
+  changeValue: string;
+};
+
+const TransactionsPerDay = () => {
+  const theme = useTheme();
+
+  return (
+    <Grid container gap={2} className={classes.wrapper}>
+      <Paper className={classes.transactions}>
+        <div className={classes.chart}>
+          <Typography>TransActions per day</Typography>
+          <DataChart type={"line"} data={lineChartData} />
+        </div>
+        <div className={classes.cardWrapper}>
+          <Card className={classes.card} variant={"outlined"}>
+            <div className={classes.cardTitle}>
+              <Typography>Total Products</Typography>
+            </div>
+            <div className={classes.cardValue}>
+              <Typography>1.275</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}>
+                428.7%
+              </Typography>
+            </div>
+          </Card>
+          <Card className={classes.card} variant={"outlined"}>
+            <div className={classes.cardTitle}>
+              <Typography>Buy-to-detail</Typography>
+            </div>
+            <div className={classes.cardValue}>
+              <Typography>4.40%</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}>
+                899.4%
+              </Typography>
+            </div>
+          </Card>
+          <Card className={classes.card} variant={"outlined"}>
+            <div className={classes.cardTitle}>
+              <Typography>Refunds</Typography>
+            </div>
+            <div className={classes.cardValue}>
+              <Typography>0</Typography>
+              <Typography color={theme.palette.success.main} fontSize={14}>
+                0
+              </Typography>
+            </div>
+          </Card>
+        </div>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default TransactionsPerDay;
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,24 +1,14 @@
-import { Box, Grid, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import classes from "@/styles/Dashboard.module.scss";
+import DataRibbon from "@/components/Dashboard/DataRibbon";
+import TransactionsPerDay from "@/components/Dashboard/TransactionsPerDay";
 
 const Dashboard = () => {
   return (
     <Box>
-      <Grid container gap={2} className={classes.topCardsContainer}>
-        <Grid>
-          <Paper className={classes.dataCard}>xs=4</Paper>
-        </Grid>
-        <Grid>
-          <Paper className={classes.dataCard}>xs=4</Paper>
-        </Grid>
-        <Grid>
-          <Paper className={classes.dataCard}>xs=4</Paper>
-        </Grid>
-      </Grid>
-      <Grid xs={12} marginY={2}>
-        <Paper className={classes.dataCard}>xs=8</Paper>
-      </Grid>
+      <DataRibbon />
+      <TransactionsPerDay />
     </Box>
   );
 };
