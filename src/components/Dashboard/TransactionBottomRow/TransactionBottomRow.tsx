9fccf7fe035455488d728d9942f63278b79f3637
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-25 18:33:33 +0100

    mockData

--- src/components/Dashboard/TransactionBottomRow/TransactionBottomRow.tsx
+++ src/components/Dashboard/TransactionBottomRow/TransactionBottomRow.tsx
@@ -2,7 +2,12 @@ import Grid from "@mui/material/Grid";
 import React from "react";
 import DataChart from "@/components/DataChart";
 import Paper from "@mui/material/Paper";
-import { doughnutChartData } from "@/components/mockData";
+import {
+  doughnutChartData1,
+  doughnutChartData2,
+  doughnutChartData3,
+  doughnutChartData4,
+} from "@/components/mockData";
 import classes from "@/styles/TransactionBottomRow.module.scss";
 
 const TransactionBottomRow = () => {
@@ -10,26 +15,26 @@ const TransactionBottomRow = () => {
     <Grid container className={classes.bottomRow}>
       <Grid>
         <Paper className={classes.dataCard}>
-          <p>Transactions per user type</p>
-          <DataChart type={"doughnut"} data={doughnutChartData} />
+          <p>TransActions per user type</p>
+          <DataChart type={"doughnut"} data={doughnutChartData1} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={classes.dataCard}>
-          <p>Transactions per user type</p>
-          <DataChart type={"doughnut"} data={doughnutChartData} />
+          <p>Social-media marketing</p>
+          <DataChart type={"doughnut"} data={doughnutChartData2} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={classes.dataCard}>
-          <p>Transactions per user type</p>
-          <DataChart type={"doughnut"} data={doughnutChartData} />
+          <p>Departments size</p>
+          <DataChart type={"doughnut"} data={doughnutChartData3} />
         </Paper>
       </Grid>
       <Grid>
         <Paper className={classes.dataCard}>
-          <p>Transactions per user type</p>
-          <DataChart type={"doughnut"} data={doughnutChartData} />
+          <p>Customer database</p>
+          <DataChart type={"doughnut"} data={doughnutChartData4} />
         </Paper>
       </Grid>
     </Grid>
--- src/components/mockData.ts
+++ src/components/mockData.ts
@@ -13,16 +13,76 @@ export const lineChartData = {
   ],
 };
 
-export const doughnutChartData = {
-  labels: ["Red", "Blue", "Yellow"],
+export const doughnutChartData1 = {
+  labels: ["Students", "Employee", "Pensioners"],
   datasets: [
     {
       label: "Transaction Dataset",
-      data: [120, 250, 80],
+      data: [150, 280, 20],
       backgroundColor: [
         "rgb(251, 255, 137)",
         "rgb(178,3,106)",
         "rgb(7, 39, 165)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export const doughnutChartData2 = {
+  labels: ["LinkedIn", "Facebook", "Twitter", "Instagram", "TikTok"],
+  datasets: [
+    {
+      label: "Transaction Dataset",
+      data: [120, 150, 80, 100, 8],
+      backgroundColor: [
+        "rgb(230, 234, 10)",
+        "rgb(178, 3, 3)",
+        "rgb(3, 178, 6)",
+        "rgb(91, 7, 165)",
+        "rgb(11, 232, 140)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export const doughnutChartData3 = {
+  labels: [
+    "Customer service",
+    "Marketing",
+    "Sales",
+    "Menagement",
+    "Refunds",
+    "HR",
+  ],
+  datasets: [
+    {
+      label: "Transaction Dataset",
+      data: [120, 10, 80, 10, 15, 15],
+      backgroundColor: [
+        "rgb(216, 167, 20)",
+        "rgb(75, 0, 90)",
+        "rgb(16, 229, 232)",
+        "rgb(7, 39, 165)",
+        "rgb(65, 1, 1)",
+        "rgb(244, 137, 104)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export const doughnutChartData4 = {
+  labels: ["Age 18-25", "Age 25-45", "Age 45+"],
+  datasets: [
+    {
+      label: "Transaction Dataset",
+      data: [120, 150, 100],
+      backgroundColor: [
+        "rgb(30, 5, 140)",
+        "rgb(25, 224, 228)",
+        "rgb(24, 224, 28)",
       ],
       hoverOffset: 4,
     },
