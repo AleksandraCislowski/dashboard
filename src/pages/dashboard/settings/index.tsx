96a9c7b784532b389a14715ee23148501c0f6863
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-29 20:10:53 +0100

    fix

--- src/pages/dashboard/settings/index.tsx
+++ src/pages/dashboard/settings/index.tsx
@@ -6,86 +6,88 @@ import {
   FormControlLabel,
   FormGroup,
   Grid,
+  Paper,
   Switch,
   Typography,
 } from "@mui/material";
+import ToggleSwitch from "@/components/settings/ToggleSwitch";
+import TextBox from "@/components/settings/TextBox";
 
 const Settings = () => {
   const [showRevenue, setShowRevenue] = React.useState(true);
-  const [showProfit, setShowProfit] = React.useState(true);
-  const [showOrders, setShowOrders] = React.useState(true);
-  const [showCustomers, setShowCustomers] = React.useState(true);
+  const [showProfit, setShowProfit] = React.useState(false);
+  const [showOrders, setShowOrders] = React.useState(false);
+  const [showCustomers, setShowCustomers] = React.useState(false);
+  const [showBilling, setShowBilling] = React.useState(false);
 
-  const handleShowRevenueChange = (event: any) => {
+  const handleShowRevenueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setShowRevenue(event.target.checked);
   };
 
-  const handleShowProfitChange = (event: any) => {
+  const handleShowProfitChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setShowProfit(event.target.checked);
   };
 
-  const handleShowOrdersChange = (event: any) => {
+  const handleShowOrdersChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setShowOrders(event.target.checked);
   };
 
-  const handleShowCustomersChange = (event: any) => {
+  const handleShowCustomersChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setShowCustomers(event.target.checked);
+  };
+
+  const handleShowBillingChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setShowBilling(event.target.checked);
   };
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    // TODO: handle form submission
   };
+
   return (
     <>
-      <h2>Settings</h2>
-      <Box>
-        <Typography variant='h4' gutterBottom>
-          Dashboard Features
+      <Typography variant='h4' gutterBottom>
+        Settings: Notifications
+      </Typography>
+      <Paper sx={{ p: 2, mb: 2 }}>
+        <Typography paragraph gutterBottom>
+          Check out all the features, that you want to add to your profile:
         </Typography>
         <form onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <FormControl component='fieldset'>
-                <FormGroup>
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={showRevenue}
-                        onChange={handleShowRevenueChange}
-                      />
-                    }
-                    label='Revenue'
-                  />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={showProfit}
-                        onChange={handleShowProfitChange}
-                      />
-                    }
-                    label='Profit'
-                  />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={showOrders}
-                        onChange={handleShowOrdersChange}
-                      />
-                    }
-                    label='Orders'
-                  />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={showCustomers}
-                        onChange={handleShowCustomersChange}
-                      />
-                    }
-                    label='Customers'
-                  />
-                </FormGroup>
-              </FormControl>
+              <FormGroup>
+                <ToggleSwitch
+                  checked={showRevenue}
+                  onChange={handleShowRevenueChange}
+                  label='Revenue'
+                />
+                <ToggleSwitch
+                  checked={showProfit}
+                  onChange={handleShowProfitChange}
+                  label='Profit'
+                />
+                <ToggleSwitch
+                  checked={showOrders}
+                  onChange={handleShowOrdersChange}
+                  label='Orders'
+                />
+                <ToggleSwitch
+                  checked={showCustomers}
+                  onChange={handleShowCustomersChange}
+                  label='Customers'
+                />
+              </FormGroup>
             </Grid>
             <Grid item xs={12}>
               <Button type='submit' variant='contained' color='primary'>
@@ -94,7 +96,25 @@ const Settings = () => {
             </Grid>
           </Grid>
         </form>
-      </Box>
+      </Paper>
+      <Paper sx={{ p: 2, mb: 2 }}>
+        <Typography
+          variant='h6'
+          gutterBottom
+          sx={{ textDecoration: "underline" }}
+        >
+          Alerts
+        </Typography>
+        <TextBox
+          subtitle='Billing and account'
+          paragraph='Transactional emails and account notifications'
+        />
+        <ToggleSwitch
+          checked={showBilling}
+          onChange={handleShowBillingChange}
+          label=''
+        />
+      </Paper>
     </>
   );
 };
