f99a7f26374b37df419b365d4b0aa3fea311ee68
aleksandra <cislowski.aleksandra@gmail.com>
2026-04-11 06:48:09 +0200

    Add style to Document component for consistent body display behavior

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: "body { display: block !important; }",
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
